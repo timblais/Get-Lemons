e8dfbbc60716e4e59e01eef2993c20c8da1e7968
Tim Blais <tblais189@gmail.com>
2022-10-09 18:19:59 -0400

    added landing page html, added router for landing page, added controller for landing page

---
+++ routers/main.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+// const authController = require('../controllers/auth') 
+const mainController = require('../controllers/main')
+// const { ensureAuth, ensureGuest } = require('../middleware/auth')
+
+router.get('/', mainController.getIndex)
+
+module.exports = router
