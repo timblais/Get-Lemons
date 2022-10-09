b65e51d2310558628ace5eefb91670a7c68e28c4
Tim Blais <tblais189@gmail.com>
2022-10-09 17:33:02 -0400

    connected mongoDB database to app

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const flash = require('express-flash')
+const logger = require('morgan')
+const connectDB = require('./config/database')
+
+require('dotenv').config({path: './config/.env'})
