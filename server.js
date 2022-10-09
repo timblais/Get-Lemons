bf34f326d4daceb65b119b46a1010ad56ac05004
Tim Blais <tblais189@gmail.com>
2022-10-09 17:48:29 -0400

    added router variables to server js, added new router files to routers folder

--- server.js
+++ server.js
@@ -7,5 +7,48 @@ const MongoStore = require('connect-mongo')(session)
 const flash = require('express-flash')
 const logger = require('morgan')
 const connectDB = require('./config/database')
+const mainRoutes = require('./routers/main')
+const loginRoutes = require('./routers/login')
+const signUpRoutes = require('./routers/signUp')
+const homeRoutes = require('./routers/home')
+const groceriesRoutes = require('./routers/groceries')
+const recipesRoutes = require('./routers/recipes')
 
 require('dotenv').config({path: './config/.env'})
+
+// Passport config
+require('./config/passport')(passport)
+
+connectDB()
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(logger('dev'))
+// Sessions
+app.use(
+    session({
+      secret: 'bigHair',
+      resave: false,
+      saveUninitialized: false,
+      store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    })
+  )
+  
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(flash())
+  
+app.use('/', mainRoutes)
+app.use('/login', loginRoutes)
+app.use('/signup', signUpRoutes)
+app.use('/home', homeRoutes)
+app.use('/groceries', groceriesRoutes)
+app.use('/recipes', recipesRoutes)
+ 
+app.listen(process.env.PORT, ()=>{
+    console.log('Server is running, you better catch it!')
+})    
