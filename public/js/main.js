0b798aa707d5a259862e1611994119752108f785
Tim Blais <tblais189@gmail.com>
2022-10-11 17:35:47 -0400

    updated file paths. added completed strikethrough styling to css file. added click functions to remove button, marking complete/incomplete

---
+++ public/js/main.js
@@ -0,0 +1,69 @@
+const deleteBtn = document.querySelectorAll('.del')
+const groceryIsIncomplete = document.querySelectorAll('span.not')
+const groceryIsComplete = document.querySelectorAll('span.completed')
+
+Array.from(deleteBtn).forEach((el)=>{
+    el.addEventListener('click', removeItem)
+})
+
+Array.from(groceryIsIncomplete).forEach((el)=>{
+    el.addEventListener('click', markComplete)
+})
+
+Array.from(groceryIsComplete).forEach((el)=>{
+    el.addEventListener('click', markIncomplete)
+})
+
+async function removeItem(){
+    const itemId = this.parentNode.dataset.id
+    try{
+        const response = await fetch('home/removeItem', {
+            method: 'put',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({
+                'itemIdFromJSFile': itemId
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    }catch(err){
+        console.log(err)
+    }
+}
+
+async function markComplete(){
+    const itemId = this.parentNode.dataset.id
+    try{
+        const response = await fetch('home/markComplete', {
+            method: 'put',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({
+                'itemIdFromJSFile': itemId
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    }catch(err){
+        console.log(err)
+    }
+}
+
+async function markIncomplete(){
+    const itemId = this.parentNode.dataset.id
+    try{
+        const response = await fetch('home/markIncomplete', {
+            method: 'put',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({
+                'itemIdFromJSFile': itemId
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    }catch(err){
+        console.log(err)
+    }
+}
